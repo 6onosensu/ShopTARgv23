d3f747105c76a76e0eec165e2829b797d6f0c165
darja <darja.portnova@gmail.com>
2024-09-25 19:33:25 +0300

    Add project files.

---
+++ ShopTARgv23.Data/ShopTARgv23Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.Core.Domain;
+
+namespace ShopTARgv23.Data
+{
+    public class ShopTARgv23Context : DbContext
+    {
+        public ShopTARgv23Context(DbContextOptions<ShopTARgv23Context> options)
+            : base(options) { }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+
+    }
+}
