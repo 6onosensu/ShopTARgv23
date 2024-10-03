6450a5e4ee04b105613d71b779823f8d2e059d98
darja <darja.portnova@gmail.com>
2024-10-03 17:10:05 +0300

    added all that files with folders, db, controller: index, indexviewmodel, Index- done?.. and so on..

---
+++ ShopTARgv23.ApplicationServices/Services/KindergartenServices.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.Core.Domain;
+using ShopTARgv23.Core.Dto;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Data;
+
+namespace ShopTARgv23.ApplicationServices.Services
+{
+    public class KindergartenServices : IKindergarten
+    {
+        private readonly ShopTARgv23Context _context;
+        public KindergartenServices(ShopTARgv23Context context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<Kindergarten> DetailsAsync(Guid id)
+        {
+            var result = await _context.Kindergarten.FirstOrDefaultAsync(x => x.Id == id);
+            return result;
+        }
+
+        public async Task<Kindergarten> Create(KindergartenDto dto)
+        {
+            Kindergarten kindergarten = new();
+
+
+            return kindergarten;
+        }
+
+        public async Task<Kindergarten> Update(KindergartenDto dto)
+        {
+            Kindergarten domain = new();
+
+
+            return domain;
+        }
+
+        public async Task<Kindergarten> Delete(Guid id)
+        {
+            var kindergarten = await _context.Kindergarten.FirstOrDefaultAsync(x => x.Id == id);
+
+
+            return kindergarten;
+        }
+    }
+}
--- ShopTARgv23/Controllers/KindergartenController.cs
+++ ShopTARgv23/Controllers/KindergartenController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv23.Data;
+using ShopTARgv23.Models.Kindergarten;
 
 namespace ShopTARgv23.Controllers
 {
     public class KindergartenController : Controller
     {
+        private readonly ShopTARgv23Context _context;
+        public KindergartenController (ShopTARgv23Context context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Kindergarten
+                .Select( x => new KindergartenIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergartenName = x.KindergartenName,
+                    Teacher = x.Teacher,
+                });
+
+            return View(result);
         }
     }
 }
