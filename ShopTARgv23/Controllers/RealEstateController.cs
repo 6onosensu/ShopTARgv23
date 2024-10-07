621f5bebf878cf1ec6361f977a61ee7b09443d3c
darja <darja.portnova@gmail.com>
2024-10-07 15:12:19 +0300

    Index realestate

---
+++ ShopTARgv23/Controllers/RealEstateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.Data;
+using ShopTARgv23.Models.RealEstate;
+
+namespace ShopTARgv23.Controllers
+{
+    public class RealEstateController : Controller
+    {
+        private readonly ShopTARgv23Context _context;
+        public RealEstateController(ShopTARgv23Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.RealEstates
+                .Select(x => new RealEstateIndexViewModel
+                {
+                    Id = x.Id,
+                    Location = x.Location,
+                    Size = x.Size,
+                    RoomNumber = x.RoomNumber,
+                    BuildingType = x.BuildingType,
+                });
+
+            return View(result);
+        }
+    }
+}
