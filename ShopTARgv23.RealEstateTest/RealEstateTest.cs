944da6758d1e2c048c09e49189aa219fad6af2e7
darja <darja.portnova@gmail.com>
2024-10-15 12:16:01 +0300

    + 2 tests

--- ShopTARgv23.RealEstateTest/RealEstateTest.cs
+++ ShopTARgv23.RealEstateTest/RealEstateTest.cs
@@ -1,5 +1,6 @@
 using ShopTARgv23.Core.Dto;
 using ShopTARgv23.Core.ServiceInterface;
+using System;
 
 namespace ShopTARgv23.RealEstateTest
 {
@@ -23,6 +24,35 @@ namespace ShopTARgv23.RealEstateTest
 
             //Assert
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task ShouldNot_GetByIdRealEstate_WhenReturnsNotEqual()
+        {
+            //Arrange
+            Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("9fdcacaa-8842-478a-ad87-472766b485c8");
+
+            //Act
+            await Svc<IRealEstate>().GetAsync(guid);
+
+            //Assert
+            Assert.NotEqual(wrongGuid, guid);
+
+        }
+    
+        [Fact]
+        public async Task Should_GetByIdRealEstate_WhenReturnsEqual()
+        {
+            //Arrange
+            Guid theGuid = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("9fdcacaa-8842-478a-ad87-472766b485c8");
+
+            //Act
+            await Svc<IRealEstate>().GetAsync(guid);
+
+            //Assert
+            Assert.Equal(theGuid, guid);
         }
     }
 }
