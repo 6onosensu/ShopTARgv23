612923f636e5e35cae89a625aa6d3dfca2f82011
darja <darja.portnova@gmail.com>
2024-10-22 12:16:20 +0300

    testing

---
+++ KindergartenTest/KindergartenTest.cs
@@ -0,0 +1,15 @@
+using ShopTARgv23.Core.Domain;
+using ShopTARgv23.Core.Dto;
+using ShopTARgv23.Core.ServiceInterface;
+
+namespace ShopTARgv23.KindergartenTest
+{
+    public class KindergartenTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
