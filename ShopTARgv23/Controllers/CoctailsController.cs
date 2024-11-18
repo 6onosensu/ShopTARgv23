c1a67e136227222fcd11e7481da9b1e1468d2b3b
darja <darja.portnova@gmail.com>
2024-11-18 15:03:10 +0200

    searcjh drink and get drink

--- ShopTARgv23/Controllers/CoctailsController.cs
+++ ShopTARgv23/Controllers/CoctailsController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv23.Core.Dto.Coctails;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Models.Coctails;
 
 namespace ShopTARgv23.Controllers
 {
     public class CoctailsController : Controller
     {
+        private readonly ICoctailsServices _drinkService;
+        public CoctailsController (ICoctailsServices drinkService)
+        {
+            _drinkService = drinkService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchDrink(DrinkViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Drink", "", new { drink = model.strDrink });
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Drink(string coctail)
+        {
+            CoctailsResultDto dto = new();
+            dto.strDrink = coctail();
+
+            _drinkService.GetDrink(dto);
+            DrinkViewModel vm = new();
+            return View(vm);
         }
     }
 }
