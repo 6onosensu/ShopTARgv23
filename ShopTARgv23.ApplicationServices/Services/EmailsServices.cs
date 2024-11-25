abc7e48e1fcfa9d65231631ef2fd0579f951e160
darja <darja.portnova@gmail.com>
2024-11-25 14:35:24 +0200

    added Sending Email

---
+++ ShopTARgv23.ApplicationServices/Services/EmailsServices.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Core.Dto;
+
+
+namespace ShopTARgv23.ApplicationServices.Services
+{
+    public class EmailsServices : IEmailsServices
+    {
+        private readonly IConfiguration _config;
+
+        public EmailsServices(IConfiguration config) 
+        { 
+            _config = config;
+        }
+
+        public void SendEmail(EmailDto dto)
+        {
+            var message = new MimeMessage();
+            var usersName = _config.GetSection("EmailUserName").Value;
+            message.From.Add(MailboxAddress.Parse(usersName));
+            message.To.Add(MailboxAddress.Parse(dto.To));
+            message.Subject = dto.Subject;
+
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = dto.Body
+            };
+            
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+                client.Authenticate(usersName, _config.GetSection("EmailPassword").Value);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ ShopTARgv23/Controllers/EmailsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.Models.Emails;
+using ShopTARgv23.Core.Dto;
+
+
+namespace ShopTARgv23.Controllers
+{
+    public class EmailsController : Controller
+    {
+        private readonly IEmailsServices _services;
+
+        public EmailsController(IEmailsServices services)
+        {
+            _services = services;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(EmailsViewModel model)
+        {
+
+            EmailDto dto = new()
+            {
+                To = model.To,
+                Subject = model.Subject,
+                Body = model.Body
+            };
+
+            _services.SendEmail(dto);
+            return RedirectToAction(nameof(Index), model);
+        }
+    }
+}
