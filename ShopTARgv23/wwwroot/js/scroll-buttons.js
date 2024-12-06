b75c1c0a8392d82e6fbece40bd7eee4baa1910a3
darja <darja.portnova@gmail.com>
2024-12-06 18:45:51 +0200

    changed styles

---
+++ ShopTARgv23/wwwroot/js/scroll-buttons.js
@@ -0,0 +1,11 @@
+﻿const scrollContainer = document.querySelector('.secondary-buttons');
+const scrollLeft = document.querySelector('.nav-scroll-left');
+const scrollRight = document.querySelector('.nav-scroll-right');
+
+scrollLeft.addEventListener('click', () => {
+    scrollContainer.scrollBy({ left: -200, behavior: 'smooth' });
+});
+
+scrollRight.addEventListener('click', () => {
+    scrollContainer.scrollBy({ left: 200, behavior: 'smooth' });
+});
