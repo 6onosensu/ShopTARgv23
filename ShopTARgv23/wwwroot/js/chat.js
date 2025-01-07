86067c4c652a9f994b03713c6cb8f07afa3349b2
darja <darja.portnova@gmail.com>
2025-01-07 12:51:00 +0200

    deleted files

--- ShopTARgv23/wwwroot/js/chat.js
+++ ShopTARgv23/wwwroot/js/chat.js
@@ -1,8 +1,7 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder()
-    .withUrl("/chatHub")
-    .build();
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
@@ -12,22 +11,16 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
-    console.log("SignalR connected!");
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
-    console.error(err.toString());
+    return console.error(err.toString());
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    console.log("Send button clicked!");
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
-        console.error(err.toString());
+        return console.error(err.toString());
     });
-
     event.preventDefault();
 });
-
-
-
